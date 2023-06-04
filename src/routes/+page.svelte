fed49a8e817f8f44d8df8ab977d43eef86c7b636
Ava Simon <ava@avamakes.dev>
2023-06-04 14:21:22 -0400

    Bootstrap and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="card" style="width: 18rem;">
+    <img src="" class="card-img-top" alt="Option 1">
+    <div class="card-body">
+      <h5 class="card-title">Option 1</h5>
+      <p class="card-text">Description</p>
+      <button class="btn btn-primary">Vote</button>
+    </div>
+  </div>
+
+  <div class="card" style="width: 18rem;">
+    <img src="" class="card-img-top" alt="Option 2">
+    <div class="card-body">
+      <h5 class="card-title">Option 2</h5>
+      <p class="card-text">Description</p>
+      <button class="btn btn-primary">Vote</button>
+    </div>
+  </div>
